d39e54a59dde60fd7fddc98a1d6350514b11bc78
Glitch (shadow-draw) <none>
2018-03-31 17:55:52 +0000

    💫🏍 Updated with Glitch

--- script.js
+++ script.js
@@ -14,8 +14,6 @@
 // remaining work:
 /*
 X building layout
-construction
-scaffolding
 X sidewalk decoration
 X sheds
 X parks (parking lots, dirt lot, grass lot)
@@ -23,10 +21,6 @@ X parking building
 X fix rivers
 X fix intersections
 X smokestack
-helipad
-aquifer?
-medians?
-fix shadow
 
 improvements:
   dont use road roof near roads
@@ -40,7 +34,7 @@ for launch:
   construction
   X helipad, swimming pool
   scaffolding
-  aquifer
+  X aquifer
 */
 
 const W = 64;
@@ -88,7 +82,7 @@ function initialize(place, w, h, init=(() => tile(0))) {
   }
 }
 initialize(map, W, H);
-initialize(meta, W, H, () => ({height:0}));
+initialize(meta, W, H, () => ({height:0, clip:1}));
 
 const sidewalks = [3,4,5,6,7,11,15,19,20,21,22,23,27,28,29,30,31,35,36,37,38,39];
 const roads = [1,2,8,9,10,16,32,40,41];
@@ -130,30 +124,29 @@ let free = (x, y, w, h) => {
     for(let j=y; j<y+h; j++) {
       if(!safe(i, j) || !empty(i, j)) {
         return false;
-      }
-    }
-  }
+  }}}
   return true;
 };
+
 let set = (x, y, w, h, tile_props, target=map) => {
   for(let i=x; i<x+w; i++) {
     for(let j=y; j<y+h; j++) {
-      Object.keys(tile_props).forEach(prop =>
-        target[i][j][prop] = tile_props[prop]);
-    }
-  }
-};
+      Object.keys(tile_props)
+        .forEach(prop => target[i][j][prop] = tile_props[prop]);
+}}};
 let toggles = {
   draw: true,
   road: false,
   height: false,
   noshade: false,
+  clip: false,
 };
 let modes = {
-  draw: (x, y, value, target) => set(x, y, 1, 1, {s:value.s}, target),
-  road: (x, y, value, target) => target==map ? set(x, y, 1, 1, {road:!meta[x][y].road}, meta) : null,
+  draw: (x, y, value, target) => (target!=map || !!meta[x][y].clip) ? set(x, y, 1, 1, {s:value.s}, target) : null,
+  road: (x, y, value, target) => target==map ? set(x, y, 1, 1, {road:true}, meta) : null,
   height: (x, y, value, target) => target==map ? set(x, y, 1, 1, {height:value}, meta) : null,
   noshade: (x, y, value, target) => target==map ? set(x, y, 1, 1, {noshade:true}, meta) : null,
+  clip: (x, y, value, target) => target==map ? set(x, y, 1, 1, {clip:value}, meta) : null,
 };
 let toggle = (what) => {
   Object.keys(what).forEach(thing => toggles[thing] = what[thing]);
@@ -167,24 +160,6 @@ const patterns = {
     [4]
   ]),
   horizontal_road: tiles([[15, 1, 1, 27]]),
-  four_way: tiles([
-    [30, 32, 32, 35],
-    [32, 32, 32, 32],
-    [32, 32, 32, 32],
-    [ 7, 32, 32,  3]
-  ]),
-  zero_way: tiles([
-    [-1, 36, 36, -1],
-    [15, 32, 32, 27],
-    [15, 32, 32, 27],
-    [-1,  4,  4, -1]
-  ]),
-  intersection: tiles([
-    [-1, 1, 1, -1],
-    [ 1, 1, 1,  1],
-    [ 1, 1, 1,  1],
-    [-1, 1, 1, -1]
-  ]),
   parkingramp: tiles([
     [150, 151]
   ]),
@@ -207,18 +182,9 @@ const tile_math = {
   [[1, 36]]: tile(32),
   [[2, 15]]: tile(32),
   [[2, 27]]: tile(32),
-  [[126, 15]]: tile(127),
-  [[126, 1]]: tile(127),
-  [[126, 27]]: tile(127),
-  [[126, 32]]: tile(127),
-  [[134, 15]]: tile(135),
-  [[134, 1]]: tile(135),
-  [[134, 27]]: tile(135),
-  [[134, 32]]: tile(135),
 };
 
-function alchemy(x, y, val, target) {
-  if(!target) target = map;
+function alchemy(x, y, val, target=map) {
   if(!val.s) return val;
    // todo: i don't think this works, but i'm not using alchemy on render targets, so w/e
   let match = tile_math[[target[x][y].s, val.s]] || tile_math[[val.s, target[x][y].s]];
@@ -234,7 +200,7 @@ function evalmodes(x, y, value, target) {
 // val_func can either be tile(15) or (i,j) => tile(15) or (i,j) => (i,j) => tile(15), etc
 function drawrect(x, y, w, h, val_func, target) {
   if(!target) { target = map; }
-  // dont worry abt it
+  // buckle up
   let evaluator = (thing) => (x, y) => 
     thing instanceof Function ? 
       evaluator(thing(x, y))(x, y) :
@@ -245,10 +211,7 @@ function drawrect(x, y, w, h, val_func, target) {
       if(safe(i, j, target)) {
         let value = alchemy(i, j, eval_val(i, j), target);
         evalmodes(i, j, value, target);
-      }
-    }
-  }
-}
+}}}}
 
 function draw(pattern, x, y) {
   pattern.forEach((row, j) =>
@@ -259,43 +222,37 @@ function draw(pattern, x, y) {
 function drawtarget(pattern, x, y) {
   pattern.forEach((row, i) =>
     row.forEach((val, j) =>
-      drawrect(x+i, y+j, 1, 1, val)));
+      val.s !== 0 && drawrect(x+i, y+j, 1, 1, val)));
 }
 
-function matchat(x, y, pattern) {
+function matchat(x, y, pattern, target=map) {
   return pattern.every((row, j) =>
     row.every((val, i) =>
-      safe(x+i, y+j) && (map[x+i][y+j].s == val || val == -1)
+      safe(x+i, y+j, target) && (target[x+i][y+j].s == val || val == -1)
     ));
 }
 
-function replaceinshape(x, y, pattern, replace, val) {
+function replaceinshape(x, y, pattern, replace, val, target=map) {
   pattern.forEach((row, j) => {
     row.forEach((col, i) => {
-      if(safe(x+i, y+j) && !empty(x+i, y+j) && col == replace) {
-        drawrect(x+i, y+j, 1, 1, val);
-      }
-    })
-  })
-}
+      if(safe(x+i, y+j, target) && !empty(x+i, y+j, target) && col == replace) {
+        drawrect(x+i, y+j, 1, 1, val, target);
+}})})}
 
-function regex(pattern, replace, val) {
+function regex(pattern, replace, val, target=map) {
   for(let y=0; y<H; y++) {
     for(let x=0; x<W; x++) {
-      if(matchat(x, y, pattern)) {
+      if(matchat(x, y, pattern, target)) {
         let matchval = val instanceof Function ? val(x, y) : val;
-        replaceinshape(x, y, pattern, replace, matchval);
-      }
-    }
-  }
-}
+        replaceinshape(x, y, pattern, replace, matchval, target);
+}}}}
 
 function drawrail() {
-  let y = rand(H/8)*8+4;
+  let y = rand(H/8)*8+5;
   
   drawrect(0, y-1, W, 1, (i,j) => !road(i,j) ? tile(54) : map[i][j]); // median
-  drawrect(0, y, W, 1, tile(126)); // top rail
-  drawrect(0, y+1, W, 1, tile(134)); // bot rail
+  drawrect(0, y, W, 1, (i,j) => meta[i][j].road ? tile(127) : tile(126)); // top rail
+  drawrect(0, y+1, W, 1, (i,j) => meta[i][j].road ? tile(135) : tile(134)); // bot rail
   drawrect(0, y+2, W, 1, (i,j) => !road(i,j) ? tile(54) : map[i][j]); // median
 }
 
@@ -367,23 +324,7 @@ function drawroad(x, y, extents) {
       }
     }
   });
-  // replace with road lines if -o- and remove lines if 3+
   drawintersection(...xy, extents);
-  // debug
-  /*
-  if(has(extents, up)) {
-    drawrect(xy[0]+4, xy[1]+4, 1, 1, tile(105));
-  }
-  if(has(extents, down)) {
-    drawrect(xy[0]+5, xy[1]+4, 1, 1, tile(106));
-  }
-   if(has(extents, left)) {
-    drawrect(xy[0]+4, xy[1]+5, 1, 1, tile(113));
-  }
-  if(has(extents, right)) {
-    drawrect(xy[0]+5, xy[1]+5, 1, 1, tile(114));
-  }
-  */
 }
 
 function drawrandroad() {
@@ -404,12 +345,12 @@ function drawrandroad() {
 // white with angle on side
 function building1(x, y, w, h) {
   // facade
-  let tall = rand(4)+2;
+  let tall = rand(2)+4;
   let b = y+h-1;
   drawrect(x, b, 1, 1, tile(160)); // leftmost floor
   drawrect(x+1, b, w-1, 1, rtile([153, 161])); // add some doors maybe
   drawrect(x, b-(tall-2), 1, tall-2, tile(152)); // leftmost wall
-  drawrect(x+1, b-(tall-2), w-1, tall-1, tile(153)); // windows
+  drawrect(x+1, b-(tall-2), w-1, tall-2, tile(153)); // windows
 
   drawrect(x, b-(tall-1), 1, 1, tile(144)); // leftmost top wall
   drawrect(x+1, b-(tall-1), w-1, 1, tile(145)); // top windows
@@ -428,12 +369,13 @@ function building1(x, y, w, h) {
   }
   toggle({noshade: false});
   
+  // mark height for shading later
   toggle({draw: false, height: true});
   drawrect(x, y, w, h, tall);
   toggle({draw: true, height: false});
 }
 
-// yellow/red, yellow roof
+// yellow/red, yellow/red/gray roof
 function building2(x, y, w, h) {
   // facade
   let tall = rand(3)+4;
@@ -468,10 +410,10 @@ function building2(x, y, w, h) {
   drawrect(x, b, w, 1, rtile([palette[0], palette[1]]));
 
   // left wall, left corner, top wall, misc
-  let lightgray = [17, 21, 24, 25, 33, 2000, 1000, 32, 32, 32, 32];
-  let lightgray_wall = [64, 65, 76];
+  let lightgray = [17, 21, 24, 25, 33, 32, 32, 32, 32];
+  let lightgrey_wall = [64, 65, 76];
   let roofpalettes = [
-    [lightgray, lightgray_wall, lightgray_wall, lightgray], // light grey
+    [lightgray, lightgrey_wall, lightgrey_wall, lightgray], // light grey
     [[80], [72], [73,74], [5000,6,51,66,67,81,82,83,88,92,103,115]], // grey
     [[97], [89], [90,91,92], [98,99,100,139,139,139,84,132,115]], // yellow
   ];
@@ -489,6 +431,7 @@ function building2(x, y, w, h) {
   }
   toggle({noshade: false});
   
+  // mark height for shading later
   toggle({draw: false, height: true});
   drawrect(x, y, w, h, tall);
   toggle({draw: true, height: false});
@@ -497,7 +440,7 @@ function building2(x, y, w, h) {
 // parking garage
 function building3(x, y, w, h) {
   // facade
-  let tall = rand(4)+3;
+  let tall = rand(2)+4;
   let dark = rand(tall-1)+1;
   let light = tall-dark;
   
@@ -552,13 +495,13 @@ function building3(x, y, w, h) {
   drawrect(x, rb-(h-2), 1, h-2, tile(80)); // left roof wall
   drawrect(x, rb-(h-1), w, 1, tile(46)); // top roof wall
   
+  // mark height for shading later
   toggle({draw: false, height: true});
   drawrect(x, y, w, h, tall);
   toggle({draw: true, height: false});
 }
 
 // grass and such
-// w: 1+ h: 1+
 function park(x, y, w, h) {
   let parking = (i, j, t) => {
     if(j == y) {
@@ -602,11 +545,50 @@ let river = (width = rand(4)+2) => (x, y, w, h) => {
   });
 }
 
+function scaffold(x, y, w, tall) {
+  drawrect(x, y-tall, w, 1, tile(104));
+  drawrect(x, y-tall+1, w, tall-1, tile(112));
+}
+
 function construction(x, y, w, h) {
+  let depth = rand(3)+2;
   let floor = [];
-  initialize(floor, w, h);
-  drawrect(0, 0, w, h, tile(139), floor);
-  drawtarget(floor, x, y);
+  initialize(floor, w, h+20);
+  drawrect(1, 10, w-1, h, tile(139000), floor);
+  drawrect(0, 10, 1, h, tile(108), floor);
+  
+  regex([[139000]], 139000, () => (i,j) => Math.random() > 0.9 ? () => {
+    drawrect(i,j+1,1,1,tile(116), floor);
+    drawrect(i+1,j+1,1,1,tile(108), floor);
+    drawrect(i+1,j,1,1,tile(109), floor);
+    return tile(58);
+  }: floor[i][j], floor); // pillars
+  regex([[58]], 58, (height=rand(6)+1) => (i,j) => { 
+    drawrect(i,j-height,1,height,tile(50000),floor);
+    drawrect(i,j-height-1,1,1,tile(6),floor);
+    return tile(58);
+  }, floor); // pillars
+  regex([[139000],[139000]], 139000, (pi,pj) => Math.random() > 0.9 ? (i,j) => j==pj ? tile(122) : tile(138) : (i,j) => floor[i][j], floor); // logs
+  regex([[139000],[139000]], 139000, (pi,pj) => Math.random() > 0.9 ? (i,j) => j==pj ? tile(121) : tile(137) : (i,j) => floor[i][j], floor); // pipes
+  regex([[137],[139000]], 139000, () => Math.random() > 0.5 ? (i,j) => { drawrect(i,j-1,1,1,tile(129),floor); return tile(137)} : tile(139000), floor); // extend logs
+  regex([[138],[139000]], 139000, () => Math.random() > 0.5 ? (i,j) => { drawrect(i,j-1,1,1,tile(130),floor); return tile(138)} : tile(139000), floor); // extend pipes
+  regex([[139000]], 139000, () => (i,j) => Math.random() > 0.4 ? rtile([123, 131, 132, 124, 140]) : floor[i][j], floor); // dirty dirt
+  
+  toggle({draw:false, clip:true});
+  drawrect(0, 0, W, H, 0);
+  drawrect(x, y+h-20, w, 20, 1);
+  toggle({draw:true, clip:false});
+  
+  scaffold(x, y+depth, w, depth);
+  drawrect(x, y+1, 1, 1, tile(120));
+  drawrect(x, y+2, 1, Math.max(0, depth-2), tile(128));
+  drawtarget(floor, x, y+depth-10);
+  
+  //todo: draw heights, and shade floor
+  
+  toggle({draw:false, clip:true});
+  drawrect(0, 0, W, H, 1);
+  toggle({draw:true, clip:false});
 }
 
 function aquifer(x, y, w, h) {
@@ -619,7 +601,6 @@ function aquifer(x, y, w, h) {
 }
 
 // raised platform that contains more stuff
-// w: 6+ h: 6+
 let raisedbit = (int) => (x, y, w, h) => {
   drawrect(x, y, w, 1, (i, j) => i==x ? tile(45) : i==x+w-1 ? tile(47) : tile(46)); // top
   drawrect(x, y+1, 1, h-2, (i, j) => j%2==0 ? tile(53) : tile(61)); // leftside
@@ -662,15 +643,14 @@ function placeriver() {
   }
   
   // water decorations (so buildings aren't considered land)
-  
   regex([[-1]], -1, (i,j) => water(i,j) && Math.random() > 0.9 ? tile(77) : map[i][j]);
   
   regex([[-1], [101]], 101, (i,j) => !water(i,j-1) && rand(2) == 0 ? tile(70) : tile(101)); // water top
   regex([[101], [-1]], 101, (i,j) => !water(i,j+1) && rand(2) == 0 ? tile(78) : tile(101)); // water bottom
 }
 
-function drawbuilding(x, y, w, h) {
-  // cut a bit off some corners
+function drawbuilding(x, y, w, h, large=false) {
+  // cut a bit off some corners?
   // pick heights for sections
   // draw roof
   // draw facade
@@ -678,26 +658,21 @@ function drawbuilding(x, y, w, h) {
   let buildings = [
     building1,
     building1,
-    building1,
-    building2,
-    building2,
-    building2,
-    building2,
-    building3,
-    building1,
-    building1,
-    building1,
     building2,
     building2,
     building2,
     building2,
     building3,
     park,
-    park,
-    raisedbit([construction]),
-    raisedbit([aquifer])
   ];
-  let building = pick(buildings)[0];
+  let largebuildings = [
+    raisedbit([construction]),
+    raisedbit([construction]),
+    raisedbit([construction]),
+    raisedbit([aquifer]),
+    park,
+  ];
+  let building = pick(large ? largebuildings : buildings)[0];
   building(x, y, w, h);
   return building;
 }
@@ -706,10 +681,10 @@ function drawalley(x, y, w, h) {
   drawrect(x, y, w, h, tile(18));
 }
 
-function placebuilding(x, y) {
-  let bw = 4;
-  let bh = 4;
-  while((empty(x+bw, y) || empty(x, y+bh)) && bw < rand(10)+3 && bh < rand(10)+3) {
+function trytoplace(x, y, large) {
+  let bw = 4+(large?2:0);
+  let bh = 4+(large?2:0);
+ while((empty(x+bw, y) || empty(x, y+bh)) && bw < rand(10)+3 && bh < rand(10)+3) {
     if(Math.random() > 0.3 && empty(x+bw, y)) {
       bw += 1;
     } else if(empty(x, y+bh)) {
@@ -718,21 +693,39 @@ function placebuilding(x, y) {
   }
   
   if(free(x, y, bw, bh)) {
-    let sidealley = false, fillright = false;
+    let sidealley = false, fillright = false, filldown = false;
     
     for(let i=0; i<4; i++) {
       if(free(x+bw+i, y, 1, bh)) {
         fillright += 1;
       }
     }
+    for(let i=0; i<4; i++) {
+      if(free(x, y+bh+i, bw, 1)) {
+        filldown += 1;
+      }
+    }
     if(fillright >= 3) {
       sidealley = true;
       fillright = false;
     }
-    let outbuild = drawbuilding(x, y, bw+(fillright?fillright:0)-(sidealley?1:0), bh);
-    if(sidealley && outbuild != park) {
-      drawalley(x+bw-1, y, 1, bh);
+    if(filldown > 2) {
+      filldown = false;
     }
+    let outbuild = drawbuilding(x, y, bw+(fillright?fillright:0)-(sidealley?1:0), bh+(filldown?filldown:0), large);
+    if(sidealley && outbuild != park) {
+      drawalley(x+bw-1, y, 1, bh+(filldown?filldown:0));
+    }
+  } else {
+    return false;
+  }
+  return true;
+}
+
+function placebuilding(x, y) {
+  let large = Math.random() > 0.7;
+  if(!trytoplace(x, y, large)) {
+    trytoplace(x, y, false);
   }
 }
 
@@ -748,8 +741,10 @@ function placebuildings() {
 function decorate_roads() {
   regex([[32],[1]], 1, tile(10)); // crosswalk
   regex([[10, -1],[1, 27]], 1, tile(8)); // stop bar
+  regex([[10],[1],[1],[1],[-1],[1],[1],[1],[1],], -1, tile(16)); // diamond
   regex([[1],[32]], 1, tile(10)); // crosswalk
   regex([[15, 1], [-1, 10]], 1, tile(8)); // stop bar
+  regex([[1],[1],[1],[1],[-1],[1],[1],[1],[10]], -1, tile(16)); // diamond
   regex([[32, 2]], 2, tile(9)); // crosswalk
   regex([[2, 32]], 2, tile(9)); // crosswalk
 }
@@ -765,19 +760,14 @@ function postshade_decorate() {
   regex([[-1, 54]], 54, () => (i,j) => shader(i-1,j) ? tile(93) : map[i][j]); // water bush
   regex([[-1, 94]], 94, () => (i,j) => shader(i-1,j) ? tile(93) : map[i][j]); // water bush
   
-  regex([[139]], 139, () => (i,j) => Math.random() > 0.6 ? rtile([123, 131, 132]) : map[i][j]); // dirty dirt 
+  regex([[139]], 139, () => (i,j) => Math.random() > 0.6 ? rtile([123, 131, 132, 124, 140]) : map[i][j]); // dirty dirt
+  
   regex([[27]], 27, () => (i,j) => Math.random() > 0.8 ? tile(20) : map[i][j]); // sidewalks
   regex([[15]], 15, () => (i,j) => Math.random() > 0.9 ? tile(20) : map[i][j]); // sidewalks
   regex([[27]], 27, () => (i,j) => Math.random() > 0.6 && has(shaders, map[i+1][j].s) ? tile(19) : map[i][j]); // sidewalks
   regex([[31]], 31, () => (i,j) => Math.random() > 0.8 ? tile(22) : map[i][j]); // sidewalks
   
   regex([[-1],[95]], 95, () => (i,j) => !has([95,94,158,159,14], map[i][j-1].s) ? tile(94) : tile(95)); // grass edge
-  
-  regex([[1000]], 1000, tile(1)); // hack to use road tiles on roofs
-  regex([[2000]], 2000, tile(2));
-  regex([[5000]], 5000, tile(5));
-  regex([[32000]], 32000, tile(32));
-  regex([[139000]], 139000, tile(139));
   
   regex([[-1], [115]], -1, () => (i,j) => Math.random() > 0.7 ? tile(115) : map[i][j]); // smokestack
   regex([[-1], [115]], -1, () => (i,j) => map[i][j].s != 115 ? tile(107) : map[i][j]); // smokestack
@@ -792,6 +782,14 @@ function postshade_decorate() {
   regex([[-1],[57]], -1, tile(49)); // container top
   
   regex([[50]], 50, () => Math.random() < 0.1 ? rtile([42,42,34]) : tile(50)); // dumpster
+  
+  regex([[1000]], 1000, tile(1)); // hack to prevent replacement until end
+  regex([[2000]], 2000, tile(2));
+  regex([[5000]], 5000, tile(5));
+  regex([[32000]], 32000, tile(32));
+  regex([[50000]], 50000, tile(50));
+  regex([[139000]], 139000, tile(139));
+  
 }
 
 function shade() {
@@ -827,11 +825,11 @@ toggle({road: true});
 
 times(rand(6)+4).each(() => drawrandroad());
 
-times(rand(3)).each(() => drawrail());
-
 decorate_roads();
 
 toggle({road: false});
+
+times(rand(3)).each(() => drawrail());
 
 placeriver();
 
